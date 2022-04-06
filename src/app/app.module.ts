4932f526e987d782302dd95b9448cf0c98bdc4ae
jbahns <javibahns@gmail.com>
2022-04-06 10:20:46 -0400

    added component templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { E404Component } from './e404/e404.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './prs/user/user.component';
+import { VendorComponent } from './prs/vendor/vendor.component';
+import { ProductComponent } from './prs/product/product.component';
+import { RequestComponent } from './prs/request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    E404Component,
+    HomeComponent,
+    MenuComponent,
+    UserComponent,
+    VendorComponent,
+    ProductComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
