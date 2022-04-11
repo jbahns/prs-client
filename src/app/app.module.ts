8e327576445866477ee90ab684f158501f73cfc5
jbahns <javibahns@gmail.com>
2022-04-11 13:58:48 -0400

    added list, detail, create, and edit components to user, vendor, product, and request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,22 @@ import { ProductComponent } from './product/product.component';
 import { RequestComponent } from './request/request.component';
 import { RequestlineComponent } from './requestline/requestline.component';
 import { AboutComponent } from './core/about/about.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { RequestListComponent } from './request/request-list/request-list.component';
+import { RequestDetailComponent } from './request/request-detail/request-detail.component';
+import { RequestCreateComponent } from './request/request-create/request-create.component';
+import { RequestEditComponent } from './request/request-edit/request-edit.component';
+import { VendorListComponent } from './vendor/vendor-list/vendor-list.component';
+import { VendorDetailComponent } from './vendor/vendor-detail/vendor-detail.component';
+import { VendorCreateComponent } from './vendor/vendor-create/vendor-create.component';
+import { VendorEditComponent } from './vendor/vendor-edit/vendor-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +42,23 @@ import { AboutComponent } from './core/about/about.component';
     ProductComponent,
     RequestComponent,
     RequestlineComponent,
-    AboutComponent
+    AboutComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductCreateComponent,
+    ProductEditComponent,
+    RequestListComponent,
+    RequestDetailComponent,
+    RequestCreateComponent,
+    RequestEditComponent,
+    VendorListComponent,
+    VendorDetailComponent,
+    VendorCreateComponent,
+    VendorEditComponent
   ],
   imports: [
     BrowserModule,
